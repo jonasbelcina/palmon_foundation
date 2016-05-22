d9bdff1cf67e1854cba7a51453cc2ba9b381e568
Zainul Abideen KT <kt.abid@gmail.com>
2016-05-22 18:17:41 +0400

    What we do

--- wp-content/themes/palmonfoundation/temaplate-what-we-do.php
+++ wp-content/themes/palmonfoundation/temaplate-what-we-do.php
@@ -9,104 +9,189 @@ $image = get_field('banner');
 $mob_img = get_field('banner_mobile'); 
 		
 ?>
-<style type="text/css">
-	.banner.about{
-		background-image: url(<?=$image?>);
-	}
-	@media (max-width: 767px) {
-		.banner.about{
-			background-image: url(<?=$mob_img?>);
-		}
-	}
-</style>
-<section class="banner about">
-	<div class="about-banner">
-		<div class="container">
+
+<section class="banner what-we-do">
+	<div class="container">
+		<div class="row">
+			<h2 class="section-header">For Women and Girls</h2>
+			<h3>We create environments where opportunities can grow</h3>
 			<div class="row">
-				<div class="col-md-5 mission"> 
-					<h1>Mission</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, .</p>
+				<div class="content">
+					<div class="col-md-6">
+						<p>We are dedicated to educating girls and empowering women to achieve their full potential physically, mentally and economically to become change makers and leaders in their community. We equip them with resources, opportunities and a global platform that facilitates networking, learning and sharing of experiences thus creating a world where every woman is able to use her voice, make her own decisions, and engage in economic opportunities</p>
+					</div>
+					<div class="col-md-6">
+						<img src="<?=get_template_directory_uri()?>/assets/images/img1.png">
+					</div> 
+					<div class="clearfix"></div>
 				</div>
-				<div class="col-md-7 who">
-					<h1>Who we are</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur.</p>
+			</div>
+		</div> 
+	</div>
+</section>
+
+
+<section class="working-model">
+	<div class="container">
+		<div class="row">
+			<h2 class="section-header">Our Working Model</h2>
+			<div class="row">
+				<div class="content">
+					<div>
+						Girls out of school
+					</div>
+					<div>
+						Girl’s Education
+					</div>
+					<div>
+						Career Development
+					</div>
+					<div>
+						Women in Leadership
+					</div>
+					<div>
+						Bringing systemic change
+					</div>
+					<div>
+						Generations of girls in school
+					</div>
+				</div>
+			</div>
+		</div> 
+	</div>
+</section>
+
+<section class="keys">
+	<div class="container">
+		<div class="row">
+			<h2 class="section-header">Keys to Transformation</h2>
+			<h3>It takes these 4 Keys to transform the lives of women &amp; girls</h3>
+			<div class="row content">
+				<ul class="nav-tabs">
+					<li class="col-xs-3 box active">
+						<a data-toggle="tab" href="#tab_1">
+							<div class="key">
+							</div>
+							<p>Peace &amp; Security</p>
+						</a>
+					</li>
+					<li class="col-xs-3 box">
+						<a data-toggle="tab" href="#tab_2">
+							<div class="key">
+							</div>
+							<p>Peace &amp; Security</p>
+						</a>
+					</li>
+					<li class="col-xs-3 box">
+						<a data-toggle="tab" href="#tab_3">
+							<div class="key">
+							</div>
+							<p>Peace &amp; Security</p>
+						</a>
+					</li>
+					<li class="col-xs-3 box">
+						<a data-toggle="tab" href="#tab_4">
+							<div class="key">
+							</div>
+							<p>Peace &amp; Security</p>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div> 
+	</div>
+	<div class="tab-content">
+		<div class="tab-pane fade in active" id="tab_1">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-2">
+						<img src="<?=get_template_directory_uri()?>/assets/images/key-1.png">
+					</div>
+					<div class="col-xs-10">
+						<h1>Peace &amp; Security</h1>
+						<p>To feel “safe” is the most basic of human desires, none more so than for any girls &amp;  women</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+		<div class="tab-pane fade in" id="tab_2">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-2 image">
+						<img src="<?=get_template_directory_uri()?>/assets/images/key-2.png">
+					</div>
+					<div class="col-xs-10">
+						<h1>Peace &amp; Security</h1>
+						<p>To feel “safe” is the most basic of human desires, none more so than for any girls &amp;  women</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+		<div class="tab-pane fade in" id="tab_3">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-2 image">
+						<img src="<?=get_template_directory_uri()?>/assets/images/key-3.png">
+					</div>
+					<div class="col-xs-10">
+						<h1>Peace &amp; Security</h1>
+						<p>To feel “safe” is the most basic of human desires, none more so than for any girls &amp;  women</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+		<div class="tab-pane fade in" id="tab_4">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-2 image">
+						<img src="<?=get_template_directory_uri()?>/assets/images/key-4.png">
+					</div>
+					<div class="col-xs-10">
+						<h1>Peace &amp; Security</h1>
+						<p>To feel “safe” is the most basic of human desires, none more so than for any girls &amp;  women</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="programmes">
+	<div class="container">
+		<div class="row">
+			<h2 class="section-header">Our Programs</h2>
+			<h3>We give Light, Hope &amp; Opportunity; They Learn<br/>Succeed and Lead Change</h3>
+			<div class="program">
+				<div class="col-md-6">
+					<h1>Lifeskills at Schools</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p> 
+				</div>
+				<div class="col-md-6">
+					<img src="<?=get_template_directory_uri()?>/assets/images/programmes.jpg">
 				</div>
 				<div class="clearfix"></div>
-			</div> 
-		</div>
-	</div>
-</section>
-
-<section class="founders">
-	<div class="container">
-		<div class="row">
-			<h2 class="section-header">Founders Speak</h2>
-
-			<?php
-				if( have_rows('founders') ):
-					while ( have_rows('founders') ) : the_row();
-			?>
-				<div class="col-lg-6 col-md-6 col-sm-6 founder-tile">
-					<?php $image = get_sub_field('image'); ?>
-					<img src="<?php echo $image; ?>" alt="" />
-					<div class="founder-content">
-						<div class="inner">
-							<h2><?php the_sub_field('name'); ?></h2>
-							<p class="title"><?php the_sub_field('designation'); ?></p>
-							<p class="title"><?php the_sub_field('organisation'); ?></p>
-							<div class="details">
-								<p><?php the_sub_field('details'); ?></p>
-							</div>
-						</div>
-					</div>
-				</div>
-			<?php
-					endwhile;
-				endif;
-			?>
-		</div>
-	</div>
-</section>
-
-<section class="the-need">
-	<div class="container">
-		<h2 class="section-header">The Need</h2>
-		<div class="row">
-			<div class="details">
-				<h2>We believe that everybody has exceptional skills, they only need an environment where they can blossom</h2>
-				<p>Educating girls and empowering women mobilizes communities to take a stand against gender disparity and to assure access to quality education and lifestyle</p>
-				<h3>Educated Girls &amp; Women WILL...</h3>
 			</div>
-			<div class="needs">
+			<div class="program">
 				<div class="col-md-6">
-					<div class="col-md-8 need"> 
-						Earn 10% More Income
-					</div>
+					<img src="<?=get_template_directory_uri()?>/assets/images/programmes.jpg">
 				</div>
 				<div class="col-md-6">
-					<div class="col-md-8 need"> 
-						Earn 10% More Income
-					</div>
+					<h1>Lifeskills at Schools</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p> 
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="program">
+				<div class="col-md-6">
+					<h1>Lifeskills at Schools</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p> 
 				</div>
 				<div class="col-md-6">
-					<div class="col-md-8 need"> 
-						Earn 10% More Income
-					</div>
-				</div>
-				<div class="col-md-6">
-					<div class="col-md-8 need"> 
-						Earn 10% More Income
-					</div>
-				</div>
-				<div class="col-md-6">
-					<div class="col-md-8 need"> 
-						Earn 10% More Income
-					</div>
-				</div>
-				<div class="col-md-6">
-					<div class="col-md-8 need"> 
-						Earn 10% More Income
-					</div>
+					<img src="<?=get_template_directory_uri()?>/assets/images/programmes.jpg">
 				</div>
 				<div class="clearfix"></div>
 			</div>
@@ -114,85 +199,52 @@ $mob_img = get_field('banner_mobile');
 	</div>
 </section>
 
-<section class="advisory-board">
+<section class="donate">
 	<div class="container">
-		<h2 class="section-header">Advisory Board</h2>
+		<h1>Together we can send a million more girls to school and create the ripple effect worldwide</h1>
 		<div class="row">
-			<div class="advisory-board-slider">
+			<a href="#">Donate Now</a>
+		</div>
+	</div>
+</section>
+
+<section class="voice">
+	<div class="container">
+		<div class="row">
+			<h2 class="section-header">Voice of Change</h2>
+
+			<?php
+			$args = array(
+				'post_type'			  => 'voices',
+				'posts_per_page'      => 1,
+				'ignore_sticky_posts' => 1,
+				);
+			$query = new WP_Query($args);
+
+			if( $query->have_posts() ): ?>
+			<div class="featured-blogs">
 				<?php
-				if( have_rows('advisory_board') ):
-					while ( have_rows('advisory_board') ) : the_row();
-				?>
-					<div class="person-tile" >
-						<div class="col-sm-6 image">
-							<?php $image = get_sub_field('image'); ?>
-							<img src="<?php echo $image; ?>" alt="" />
-						</div>
-						<div class="col-sm-6 content">
-							<div class="inner">
-								<h2><?php the_sub_field('name'); ?></h2>
-								<p class="title"><?php the_sub_field('designation'); ?></p>
-								<p><?php the_sub_field('description'); ?></p>
-							</div>
-						</div>
-						<div class="clearfix"></div>
+				while( $query->have_posts() ) : $query->the_post(); ?>
+					<div class="col-md-6">
+						<?php the_post_thumbnail('full');?>
 					</div>
-				<?php
-						endwhile;
-					endif;
-				?>
-			</div> 
-		</div>
-	</div>
-</section>
-
-<section class="our-partners">
-	<div class="container">
-		<h2 class="section-header">Our Partners</h2>
-		<div class="row">
-			<?php $images = get_field('partners');
-			if( $images ): ?>
-			    <div class="partners-slider">
-			        <?php foreach( $images as $image ): ?>
-			            <img src="<?php echo $image['url'] ?>" alt="<?php echo $image['alt'] ?>" />
-			        <?php endforeach; ?>
-			    </div>
-			<?php endif; ?>
-		</div>
-	</div>
-</section>
-
-<section class="impact">
-	<div class="container">
-		<h2 class="section-header">Impact</h2>
-		<div class="row">
-			<div class="col-md-4 single">
-				<div class="image">
-					<img src="<?=get_template_directory_uri()?>/assets/images/book.png">
-				</div>
-				<h2>Earn 10% More Income</h2>
-				<p>We educate girls and empower women by leveraging the latest tools and technology and our collaborative learning community.</p>
-			</div> 
-			<div class="col-md-4 single">
-				<div class="image">
-					<img src="<?=get_template_directory_uri()?>/assets/images/prize.png">
-				</div>
-				<h2>Championing Change</h2>
-				<p>We equip our members with cutting-edge knowledge, resources and opportunities to become agents of change in their communities.</p>
-			</div> 
-			<div class="col-md-4 single">
-				<div class="image">
-					<img src="<?=get_template_directory_uri()?>/assets/images/globe.png">
-				</div>
-				<h2>Massive Presence</h2>
-				<p>Palmon Foundation is leaping for women's economic empowerment  with like minded members, organizations and companies.</p>
+					<div class="col-md-6">
+						<h1><?php the_title(); ?></h1>
+						<p><?php echo shortenText(get_the_excerpt(), 195, ''); ?></p> 
+					</div>
+					<div class="clearfix"></div>
+				<?php 
+				endwhile; ?>
 			</div>
-			<div class="clearfix"></div>
-			<a href="#" class="join">Join US Now</a>
+			<?php
+			endif;
+
+			wp_reset_postdata();
+			?>
+			
 		</div>
 	</div>
 </section>
-
 
 <?php get_footer(); ?>
 
