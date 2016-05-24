8b8f7deb345292fc5a79a46e0d68b4312ce7b463
Jonas Belcina <jonasbelcina10@gmail.com>
2016-05-24 21:10:36 +0400

    Fix styles

--- wp-content/themes/palmonfoundation/temaplate-what-we-do.php
+++ wp-content/themes/palmonfoundation/temaplate-what-we-do.php
@@ -5,23 +5,24 @@ Template Name: What we do
 
 get_header(); 
 			
-$image = get_field('banner'); 
-$mob_img = get_field('banner_mobile'); 
+$image = get_field('banner');
+$mob_img = get_field('banner_mobile');
 		
 ?>
 
 <section class="banner what-we-do">
 	<div class="container">
 		<div class="row">
-			<h2 class="section-header">For Women and Girls</h2>
-			<h3>We create environments where opportunities can grow</h3>
+			<h2 class="section-header"><?php the_field('fwg_heading'); ?></h2>
+			<h3><?php the_field('fwg_subheading'); ?></h3>
 			<!-- <div class="row"> -->
 				<div class="content">
 					<div class="col-md-6">
-						<p>We are dedicated to educating girls and empowering women to achieve their full potential physically, mentally and economically to become change makers and leaders in their community. We equip them with resources, opportunities and a global platform that facilitates networking, learning and sharing of experiences thus creating a world where every woman is able to use her voice, make her own decisions, and engage in economic opportunities</p>
+						<?php echo wpautop(get_field('fwg_text_content')); ?>
 					</div>
 					<div class="col-md-6">
-						<img src="<?=get_template_directory_uri()?>/assets/images/img1.png">
+						<?php $fwg_img = get_field('fwg_image'); ?>
+						<img src="<?php echo $fwg_img['url']; ?>" alt="<?php echo $fwg_img['alt']; ?>" />
 					</div> 
 					<div class="clearfix"></div>
 				</div>
@@ -62,107 +63,56 @@ $mob_img = get_field('banner_mobile');
 <section class="keys">
 	<div class="container">
 		<div class="row">
-			<h2 class="section-header">Keys to Transformation</h2>
-			<h3>It takes these 4 Keys to transform the lives of women &amp; girls</h3>
+			<h2 class="section-header"><?php the_field('keys_heading'); ?></h2>
+			<h3><?php the_field('keys_subheading'); ?></h3>
 			<div class="content">
-				<ul class="nav-tabs" id="sync1">
-					<li class="col-xs-3 key1 box active">
-						<a data-toggle="tab" href="#tab_1">
-							<div class="key">
-							</div>
-							<p>Peace &amp; Security</p>
-						</a>
-					</li>
-					<li class="col-xs-3 key2 box">
-						<a data-toggle="tab" href="#tab_2">
-							<div class="key">
-							</div>
-							<p>Proper Health Care</p>
-						</a>
-					</li>
-					<li class="col-xs-3 key3 box">
-						<a data-toggle="tab" href="#tab_3">
-							<div class="key">
-							</div>
-							<p>Quality Education</p>
-						</a>
-					</li>
-					<li class="col-xs-3 key4 box">
-						<a data-toggle="tab" href="#tab_4">
-							<div class="key">
-							</div>
-							<p>Financial Independence</p>
-						</a>
-					</li>
-				</ul>
+				<?php if( have_rows('keys_keys') ): ?>
+					<?php $ctr = 1; ?>
+					<ul class="nav-tabs" id="sync1">
+						<?php while ( have_rows('keys_keys') ) : the_row(); ?>
+							<li class="col-xs-3 key<?php echo $ctr; ?> box <?php if($ctr == 1) { echo 'active'; } ?>">
+								<a data-toggle="tab" href="#tab_<?php echo $ctr; ?>">
+									<div class="key">
+									</div>
+									<?php echo get_sub_field('title'); ?>
+								</a>
+							</li>
+							<?php $ctr++; ?>
+						<?php endwhile; ?>
+					</ul>
+				<?php endif; ?>
 			</div>
 		</div> 
 	</div>
 	<div class="tab-content" id="sync2">
-		<div class="tab-pane fade in active" id="tab_1">
-			<div class="container">
-				<div class="row">
-					<div class="col-xs-2">
-						<img src="<?=get_template_directory_uri()?>/assets/images/key-1.png">
+		<?php if( have_rows('keys_keys') ): ?>
+			<?php $ctr = 1; ?>
+			<?php while ( have_rows('keys_keys') ) : the_row(); ?>
+					<div class="tab-pane fade in <?php if($ctr == 1) { echo 'active'; } ?>" id="tab_<?php echo $ctr; ?>">
+						<div class="container">
+							<div class="row">
+								<div class="col-xs-2">
+									<img src="<?=get_template_directory_uri()?>/assets/images/key-1.png">
+								</div>
+								<div class="col-xs-10">
+									<h1><?php echo get_sub_field('title'); ?></h1>
+									<?php echo wpautop(get_sub_field('content')); ?>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+						</div>
 					</div>
-					<div class="col-xs-10">
-						<h1>Peace &amp; Security</h1>
-						<p>To feel “safe” is the most basic of human desires, none more so than for any girls &amp;  women</p>
-					</div>
-					<div class="clearfix"></div>
-				</div>
-			</div>
-		</div>
-		<div class="tab-pane fade in" id="tab_2">
-			<div class="container">
-				<div class="row">
-					<div class="col-xs-2 image">
-						<img src="<?=get_template_directory_uri()?>/assets/images/key-2.png">
-					</div>
-					<div class="col-xs-10">
-						<h1>Proper Health Care</h1>
-						<p>To feel “safe” is the most basic of human desires, none more so than for any girls &amp;  women</p>
-					</div>
-					<div class="clearfix"></div>
-				</div>
-			</div>
-		</div>
-		<div class="tab-pane fade in" id="tab_3">
-			<div class="container">
-				<div class="row">
-					<div class="col-xs-2 image">
-						<img src="<?=get_template_directory_uri()?>/assets/images/key-3.png">
-					</div>
-					<div class="col-xs-10">
-						<h1>Quality Education</h1>
-						<p>To feel “safe” is the most basic of human desires, none more so than for any girls &amp;  women</p>
-					</div>
-					<div class="clearfix"></div>
-				</div>
-			</div>
-		</div>
-		<div class="tab-pane fade in" id="tab_4">
-			<div class="container">
-				<div class="row">
-					<div class="col-xs-2 image">
-						<img src="<?=get_template_directory_uri()?>/assets/images/key-4.png">
-					</div>
-					<div class="col-xs-10">
-						<h1>Financial Independence</h1>
-						<p>To feel “safe” is the most basic of human desires, none more so than for any girls &amp;  women</p>
-					</div>
-					<div class="clearfix"></div>
-				</div>
-			</div>
-		</div>
+			<?php $ctr++; ?>
+		<?php endwhile; ?>
+	<?php endif; ?>
 	</div>
 </section>
 
 <section class="programmes">
 	<div class="container">
 		<div class="row">
-			<h2 class="section-header">Our Programs</h2>
-			<h3>We give Light, Hope &amp; Opportunity; They Learn<br/>Succeed and Lead Change</h3>
+			<h2 class="section-header"><?php the_field('wwdop_heading'); ?></h2>
+			<h3><?php the_field('wwdop_subheading'); ?></h3>
 			<div class="program">
 				<div class="col-md-6">
 					<h1>Lifeskills at Schools</h1>
@@ -174,12 +124,12 @@ $mob_img = get_field('banner_mobile');
 				<div class="clearfix"></div>
 			</div>
 			<div class="program">
-				<div class="col-md-6">
-					<img src="<?=get_template_directory_uri()?>/assets/images/programmes.jpg">
-				</div>
-				<div class="col-md-6">
+				<div class="col-md-6 pull-right">
 					<h1>Lifeskills at Schools</h1>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p> 
+				</div>
+				<div class="col-md-6">
+					<img src="<?=get_template_directory_uri()?>/assets/images/programmes.jpg">
 				</div>
 				<div class="clearfix"></div>
 			</div>
