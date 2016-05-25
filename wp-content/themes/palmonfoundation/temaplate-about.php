60ad0b26656b46396b464c52cb12ef2725e8a3bb
Jonas Belcina <jonasbelcina10@gmail.com>
2016-05-25 10:34:27 +0400

    Add ACF for banner

--- wp-content/themes/palmonfoundation/temaplate-about.php
+++ wp-content/themes/palmonfoundation/temaplate-about.php
@@ -24,12 +24,12 @@ $mob_img = get_field('banner_mobile');
 		<div class="container">
 			<div class="row">
 				<div class="col-md-5 mission"> 
-					<h1>Mission</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, .</p>
+					<h1><?php the_field('banner_left_col_header'); ?></h1>
+					<?php echo wpautop(get_field('banner_left_col_content')); ?>
 				</div>
 				<div class="col-md-7 who">
-					<h1>Who we are</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur.</p>
+					<h1><?php the_field('banner_right_col_header'); ?></h1>
+					<?php echo wpautop(get_field('banner_right_col_content')); ?>
 				</div>
 				<div class="clearfix"></div>
 			</div> 
